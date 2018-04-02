51cea1e004a0f5c43a06938a9fcd8c3211a19d15
Dustin Harris <dustin.harris@outlook.com>
2018-04-02 16:02:54 -0700

    Started work on reusable modal dialog

---
+++ Assets/Scripts/UI/ModalPanel.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ModalPanel : MonoBehaviour {
+
+    public Text question;
+    public Image iconImage;
+    public Button firstButtonInPanel;
+    public Button secondButtonInPanel;
+    public Button thirdButtonInPanel;
+    public GameObject modalPanelObject;
+
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+            {
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your scene.");
+            }
+        }
+
+        return modalPanel;
+    }
+
+    // Yes/No/Cancel: A string, a Yes event, a No event, and a Cancel event
+    public void Choice(string question, UnityAction firstButtonEvent, UnityAction secondButtonEvent, UnityAction thirdButtonEvent)
+    {
+        modalPanelObject.SetActive(true);
+
+        firstButtonInPanel.onClick.RemoveAllListeners();
+        firstButtonInPanel.onClick.AddListener(firstButtonEvent);
+        firstButtonInPanel.onClick.AddListener(ClosePanel);
+
+        secondButtonInPanel.onClick.RemoveAllListeners();
+        secondButtonInPanel.onClick.AddListener(secondButtonEvent);
+        secondButtonInPanel.onClick.AddListener(ClosePanel);
+
+        thirdButtonInPanel.onClick.RemoveAllListeners();
+        thirdButtonInPanel.onClick.AddListener(thirdButtonEvent);
+        thirdButtonInPanel.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive(false);
+        firstButtonInPanel.gameObject.SetActive(true);
+        secondButtonInPanel.gameObject.SetActive(true);
+        thirdButtonInPanel.gameObject.SetActive(true);
+    }
+
+    void ClosePanel()
+    {
+        modalPanelObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/TestModalWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TestModalWindow : MonoBehaviour {
+
+    private ModalPanel modalPanel;
+    private DisplayManager displayManager;
+
+    private UnityAction myFirstAction;
+    private UnityAction mySecondAction;
+    private UnityAction myThirdAction;
+
+    void Awake()
+    {
+        modalPanel = ModalPanel.Instance();
+        displayManager = DisplayManager.Instance();
+
+        myFirstAction = new UnityAction(TestFirstButtonFunction);
+        mySecondAction = new UnityAction(TestSecondButtonFunction);
+        myThirdAction = new UnityAction(TestThirdButtonFunction);
+    }
+    
+    // Send to the Modal Panel to set up the Buttons and Functions to call
+    public void Test123()
+    {
+        modalPanel.Choice("Would you like a poke in the eye?\nHow about with a sharp stick?", myFirstAction, mySecondAction, myThirdAction);
+    }
+
+    // These are wrapped into UnityActions
+    void TestFirstButtonFunction()
+    {
+        displayManager.DisplayMessage("Heck yeah!");
+    }
+
+    void TestSecondButtonFunction()
+    {
+        displayManager.DisplayMessage("No way, Jose!");
+    }
+
+    void TestThirdButtonFunction()
+    {
+        displayManager.DisplayMessage("I give up!");
+    }
+}
